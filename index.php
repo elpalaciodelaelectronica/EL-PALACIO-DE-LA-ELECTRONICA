632e4b8f9c9c8a4c7c95ed5412a41221b1d733f1
elpalaciodelaelectronica <30351670+elpalaciodelaelectronica@users.noreply.github.com>
2017-07-21 13:58:01 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
-<html style="background-color#FF0000">
+<html lang="en-US">
 <head>
-  
+<title>HTML input accept Attribute</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="Keywords" content="HTML,CSS,JavaScript,SQL,PHP,jQuery,XML,DOM,Bootstrap,Web development,W3C,tutorials,programming,training,learning,quiz,primer,lessons,references,examples,source code,colors,demos,tips">
+<meta name="Description" content="Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, and XML.">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="/w3css/4/w3.css">
 </head>
-<body>
+<body style="background-color:#44DD22">
+  asdasdasdasd
+<div style="width:400px;heigth:400px;background-color:#FF33AA">
+</div>
 </body>
 </html>
